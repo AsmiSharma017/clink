f158f2429d17c9048621f0e29f345797cf61f030
Asmi <asmi@Asmis-MacBook-Air.local>
2026-05-21 14:14:17 +0530

    fix: add catch-all route for React Router

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ const io = new Server(server, { cors: { origin: '*' } });
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 app.use('/api/auth', authRoutes);
 app.use('/api/export', exportRoutes);
 
@@ -50,5 +52,7 @@ const startServer = async () => {
 if (process.env.NODE_ENV !== 'test') {
   startServer();
 }
+
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")));
 
 module.exports = app;
